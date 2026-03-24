9c2ecf6008a61621760fdc5e435b6f38bc440385
smccomics5-cyber <smccomics5@gmail.com>
2026-03-24 12:00:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     basic.showString("Shivang!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("SMC!")
+})
+basic.forever(function () {
+	
 })
